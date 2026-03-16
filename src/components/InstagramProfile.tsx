cafdaf9f6b458574c0af40592e0f97e64d162abc
AJ Ferron <alvn.frrn@gmail.com>
2026-03-16 01:16:33 -0400

    add ig profile embed (missing change)

---
+++ src/components/InstagramProfile.tsx
@@ -0,0 +1,31 @@
+'use client';
+import { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    instgrm?: { Embeds: { process: () => void } };
+  }
+}
+
+export default function InstagramProfile() {
+  useEffect(() => {
+    if (window.instgrm) {
+      window.instgrm.Embeds.process();
+    } else {
+      const s = document.createElement('script');
+      s.src = 'https://www.instagram.com/embed.js';
+      s.async = true;
+      document.body.appendChild(s);
+    }
+  }, []);
+
+  return (
+    <div className="pb-12">
+      <blockquote
+        className="instagram-media w-128"
+        data-instgrm-permalink="https://www.instagram.com/elite_eats_inc/"
+        data-instgrm-version="14"
+      />
+    </div>
+  );
+}
